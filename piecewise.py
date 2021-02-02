62356c45d9e2e6dc288166d711cefa4fb978268d
Hamed <sghavamnia@cs.stonybrook.edu>
2021-02-02 01:48:51 -0500

    added using dynamically loaded libraries for library debloating instead of ldd

--- piecewise.py
+++ piecewise.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import re
 
 sys.path.insert(0, './python-utils/')
 
@@ -20,10 +21,12 @@ class Piecewise:
         self.logger = logger
 
     def cleanLib(self, libName):
+        self.logger.debug("cleanLib libName input: %s", libName)
         if ( ".so" in libName ):
             libName = re.sub("-.*so",".so",libName)
             libName = libName[:libName.index(".so")]
             #libName = libName + ".so"
+        self.logger.debug("cleanLib libName output: %s", libName)
         return libName
 
     def createCompleteGraph(self, exceptList=list()):
@@ -145,7 +148,16 @@ class Piecewise:
             indirectFunctionToSyscallMap[startNode] = accessibleSyscalls
         return indirectFunctionToSyscallMap
 
-    def createCompleteGraphWithoutBinary(self, exceptList=list(), altLibPath=None):
+    # This function can used instead of createCompleteGraph when we don't have access or do not require the 
+    # callgraph of the binary itself (all the code in the binary is required)
+    # Any imported function in the binary will be used as the starting points of this complete graph
+    # The graph in this case will only consist of the libraries
+    # TODO (02/2021): We have a bug here, we shouldn't check libraries with callgraphs and without in the same loop
+    # We should either first create the complete graph for all libraries with a callgraph and then perform 
+    # another loop for the libraries without a callgraph, or extract the start node from all libraries without 
+    # a callgraph and then consider those start nodes for the complete graph
+    # This bug also applies to the original createCompleteGraph
+    def createCompleteGraphWithoutBinary(self, exceptList=list(), altLibPath=None, procLibraryDict=dict()):
         '''TODO
         1. Extract required libraries from binary (ldd)
         2. Find call graph for each library from specified folder (input: callgraph folder)
@@ -159,7 +171,11 @@ class Piecewise:
         libcRelatedList = ["ld", "libc", "libdl", "libcrypt", "libnss_compat", "libnsl", "libnss_files", "libnss_nis", "libpthread", "libm", "libresolv", "librt", "libutil", "libnss_dns"]
         libraryCfgGraphs = dict()
         librarySyscalls = set()  #Only for libraries which we DO NOT have the CFG
-        libraryToPathDict = util.readLibrariesWithLdd(self.binaryPath)
+        if ( procLibraryDict and len(procLibraryDict) != 0 ):
+            libraryToPathDict = procLibraryDict
+            self.logger.debug("library debloating createCompleteGraphWithoutBinary library name and path received, no need for ldd")
+        else:
+            libraryToPathDict = util.readLibrariesWithLdd(self.binaryPath)
 
         startNodeToLibDict = dict()
 
@@ -172,60 +188,84 @@ class Piecewise:
         if ( result == -1 ):
             self.logger.error("Failed to create graph for input: %s", self.libcCfgPath)
             sys.exit(-1)
-        
+
+        # Fixing bug: should create callgraph first, then extract start nodes and find accessible system calls        
         for libraryName, libPath in libraryToPathDict.items():
-            self.logger.info("Checking library: %s", libraryName)
-            libraryCfgFileName = self.cleanLib(libraryName) + ".callgraph.out"
-            libraryCfgFilePath = self.cfgPath + "/" + libraryCfgFileName
-            if ( libraryName not in libcRelatedList and libraryName not in exceptList ):
-                altBinaryPath = self.existsInAltPath(libraryName, altLibPath)
-                if ( os.path.isfile(libraryCfgFilePath) ):
-                    #We have the CFG for this library
-                    self.logger.info("The library call graph exists for: %s", libraryName)
+            if ( ".so" in libPath ):
+                self.logger.info("Checking library: %s", libraryName)
+                libraryFullName = libraryName
+                libraryCfgFileName = self.cleanLib(libraryName) + ".callgraph.out"
+                libraryCfgFilePath = self.cfgPath + "/" + libraryCfgFileName
+                libraryName = self.cleanLib(libraryName)
+                if ( libraryName not in libcRelatedList and libraryName not in exceptList ):
+                    #altBinaryPath = self.existsInAltPath(libraryName, altLibPath)  #Using the cleaned version causes problem for libapr
+                    altBinaryPath = self.existsInAltPath(libraryFullName, altLibPath)
+                    if ( os.path.isfile(libraryCfgFilePath) ):
+                        #We have the CFG for this library
+                        self.logger.info("The library call graph exists for: %s", libraryName)
 
-                    libraryGraph = graph.Graph(self.logger)
-                    libraryGraph.createGraphFromInput(libraryCfgFilePath)
-                    self.logger.info("Finished create graph object for library: %s", libraryName)
-                    libraryStartNodes = libraryGraph.extractStartingNodes()
-                    self.logger.info("Finished extracting start nodes for library: %s", libraryName)
+                        libraryGraph = graph.Graph(self.logger)
+                        libraryGraph.createGraphFromInput(libraryCfgFilePath)
+                        self.logger.info("Finished create graph object for library: %s", libraryName)
+                        libraryStartNodes = libraryGraph.extractStartingNodes()
+                        self.logger.info("Finished extracting start nodes for library: %s", libraryName)
 
-                    #We're going keep a copy of the full library call graph, for later stats creation
-                    libraryCfgGraphs[libraryName] = libraryGraph
+                        #We're going keep a copy of the full library call graph, for later stats creation
+                        libraryCfgGraphs[libraryName] = libraryGraph
 
-                    #(Step 3 in todo list): We're going to make a smaller graph containing only start nodes and end nodes
-                    #libraryStartToEndGraph = graph.Graph(self.logger)
+                        #(Step 3 in todo list): We're going to make a smaller graph containing only start nodes and end nodes
+                        #libraryStartToEndGraph = graph.Graph(self.logger)
 
-                    for startNode in libraryStartNodes:
-                        if ( startNodeToLibDict.get(startNode, None) ):
-                            self.logger.warning("library startNode seen in more than one library: %s and %s", libraryName, startNodeToLibDict[startNode])
-                        startNodeToLibDict[startNode] = libraryName
-                        leaves = libraryGraph.getLeavesFromStartNode(startNode, list(), list())
-                        for leaf in leaves:
-                            #self.logger.debug("Adding edge %s->%s from library: %s to complete graph.", startNode, leaf, libraryName)
-                            #libraryStartToEndGraph.addEdge(startNode, leaf)
-                            completeGraph.addEdge(startNode, leaf)
-                    #libraryGraphs[libraryName] = libraryStartToEndGraph
-                elif ( os.path.isfile(libPath) or altBinaryPath ):
-                    #We don't have the CFG for this library, all exported functions will be considered as starting nodes in our final graph
-                    self.logger.info("The library call graph doesn't exist, considering all imported functions for: %s", libraryName)
-                    path = libPath if os.path.isfile(libPath) else altBinaryPath
-                    self.logger.info("path: %s", path)
-                    libraryProfiler = binaryAnalysis.BinaryAnalysis(path, self.logger)
-                    directSyscallSet, successCount, failedCount  = libraryProfiler.extractDirectSyscalls()
-                    indirectSyscallSet = libraryProfiler.extractIndirectSyscalls(completeGraph)
-
-                    librarySyscalls.update(directSyscallSet)
-                    librarySyscalls.update(indirectSyscallSet)
+                        for startNode in libraryStartNodes:
+                            if ( startNodeToLibDict.get(startNode, None) ):
+                                self.logger.warning("library startNode seen in more than one library: %s and %s", libraryName, startNodeToLibDict[startNode])
+                            startNodeToLibDict[startNode] = libraryName
+                            leaves = libraryGraph.getLeavesFromStartNode(startNode, list(), list())
+                            for leaf in leaves:
+                                #self.logger.debug("Adding edge %s->%s from library: %s to complete graph.", startNode, leaf, libraryName)
+                                #libraryStartToEndGraph.addEdge(startNode, leaf)
+                                completeGraph.addEdge(startNode, leaf)
+                        #libraryGraphs[libraryName] = libraryStartToEndGraph
+                    else:
+                        self.logger.warning("Skipping library: %s because down't have callgraph: %s", libraryName, libraryCfgFilePath)
                 else:
-                    self.logger.warning("Skipping library: %s because path: %s doesn't exist", libraryName, libPath)
+                    self.logger.info("Skipping except list library: %s", libraryName)
             else:
-                self.logger.info("Skipping except list library: %s", libraryName)
+                self.logger.info("Skipping non-library: %s in binary dependencies (can happen because of /proc", libraryName)
+
+        for libraryName, libPath in libraryToPathDict.items():
+            if ( ".so" in libPath ):
+                self.logger.info("Checking library: %s", libraryName)
+                libraryFullName = libraryName
+                libraryCfgFileName = self.cleanLib(libraryName) + ".callgraph.out"
+                libraryCfgFilePath = self.cfgPath + "/" + libraryCfgFileName
+                libraryName = self.cleanLib(libraryName)
+                if ( libraryName not in libcRelatedList and libraryName not in exceptList ):
+                    #altBinaryPath = self.existsInAltPath(libraryName, altLibPath)  #Using the cleaned version causes problem for libapr
+                    altBinaryPath = self.existsInAltPath(libraryFullName, altLibPath)
+                    if ( os.path.isfile(libPath) or altBinaryPath ):
+                        #We don't have the CFG for this library, all exported functions will be considered as starting nodes in our final graph
+                        self.logger.info("The library call graph doesn't exist, considering all imported functions for: %s", libraryName)
+                        path = libPath if os.path.isfile(libPath) else altBinaryPath
+                        self.logger.info("path: %s", path)
+                        libraryProfiler = binaryAnalysis.BinaryAnalysis(path, self.logger)
+                        directSyscallSet, successCount, failedCount  = libraryProfiler.extractDirectSyscalls()
+                        indirectSyscallSet = libraryProfiler.extractIndirectSyscalls(completeGraph)
+
+                        librarySyscalls.update(directSyscallSet)
+                        librarySyscalls.update(indirectSyscallSet)
+                    else:
+                        self.logger.warning("Skipping library: %s because path: %s doesn't exist", libraryName, libPath)
+                else:
+                    self.logger.info("Skipping except list library: %s", libraryName)
+            else:
+                self.logger.info("Skipping non-library: %s in binary dependencies (can happen because of /proc", libraryName)
 
         return completeGraph, librarySyscalls, libraryCfgGraphs
 
-    def extractAccessibleSystemCallsFromBinary(self, startNodes, exceptList=list(), altLibPath=None):
+    def extractAccessibleSystemCallsFromBinary(self, startNodes, exceptList=list(), altLibPath=None, procLibraryDict=dict()):
         self.logger.info("Extracting acessible system calls from binary")
-        completeGraph, librarySyscalls, libraryCfgGraphs = self.createCompleteGraphWithoutBinary(exceptList, altLibPath)
+        completeGraph, librarySyscalls, libraryCfgGraphs = self.createCompleteGraphWithoutBinary(exceptList, altLibPath, procLibraryDict)
 
         accessibleSyscalls = set()
         for startNode in startNodes:
@@ -243,11 +283,13 @@ class Piecewise:
     def existsInAltPath(self, libraryName, altLibPath):
         if altLibPath is None:
             return None
+        self.logger.debug("existsInAltPath looking for: %s", libraryName)
 
         contents = os.listdir(altLibPath)
 
         for c in contents:
             if c.find(libraryName) != -1:
+                self.logger.debug("existsInAltPath returning: %s", (os.path.abspath(altLibPath) + "/" + c))
                 return os.path.abspath(altLibPath) + "/" + c
 
         return None
